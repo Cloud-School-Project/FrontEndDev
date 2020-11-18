f94efe418d714602f8b4690b920056aeef783abf
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2020-11-18 11:50:11 -0700

    added validations

---
+++ front-end-dev/src/validations/formSchemaRegistration.js
@@ -0,0 +1,33 @@
+import * as yup from "yup";
+
+export default yup.object().shape({
+    username: yup
+        .string()
+        .required("Username is required")
+        .min(3, "Username must be 3 chars long"),
+    email: yup
+        .string()
+        .email("Must be valid email address")
+        .required("Must include email address"),
+    password: yup
+        .string()
+        .required('Password is required'),
+    confirmPassword: yup
+        .string()
+        .required('Confirmation Password is required'),
+    role: yup
+        .string()
+        .oneOf(["admin", "student", "volunteer"], "Role is required"),
+    availability: yup
+        .boolean()
+        .oneOf([true], 'Must at least choose one day for availability')
+        .required(),
+    country: yup
+        .string()
+        .required("Country is required")
+        .min(4, "Country must be 4 characters or longer"),
+    usernameLogin: yup
+        .string(),
+    passwordLogin: yup
+        .string(),
+});
